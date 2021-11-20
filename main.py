e953d2f457ff5538ca3316f6896c02c680969577
Francesco Romeo <francesco_romeo@me.com>
2021-11-20 10:26:45 +0100

    locations config and pyplot requirement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import simpy
+import configparser
 
 from distr_center import DistrCenter
 from hospital import Hospital
@@ -6,6 +7,8 @@ from vehicles.drone import Drone
 from vehicles.ambulance import Ambulance
 
 def main():
+    map = configparser.ConfigParser()
+    map.read('resources/locations.ini')
     env = simpy.Environment()
 
     drones = []
@@ -16,14 +19,13 @@ def main():
     for i in range(5):
         ambulances.append(Ambulance(i))
 
-    dc = DistrCenter(env, 0, -0.27787343210717297, 36.07028912825954, drones, ambulances)
+    dc = DistrCenter(env, 0, map['DST_CENTER']['lat'], map['DST_CENTER']['long'], drones, ambulances)
 
     # hosp0 = Hospital(env, 0, 10, 20, dc)
     # hosp1 = Hospital(env, 1, 50, 60, dc)
 
     hosp0 = Hospital(env, 0, -1.3042120313089076, 36.806186441462806, dc)
     hosp1 = Hospital(env, 0, -4.06218700915241, 39.6776812700527, dc)
-
 
     env.run(until=500)
 
