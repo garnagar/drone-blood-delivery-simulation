2ee64bbc4d4b1f416b026e5896a5dd46876c1572
Francesco Romeo <francesco_romeo@me.com>
2021-11-21 17:05:17 +0100

    drone logic skeleton

--- distr_center.py
+++ distr_center.py
@@ -1,13 +1,9 @@
-"""
-Project: Drone Blood Delivery Simulator
-File: DistribCenter.py
-Author:  Lukas Kyzlik
-"""
 import numpy as np
 import simpy
 
 from vehicles import drone
 from vehicles import ambulance
+from vehicles.config import DRONE_TOTAL_CARRYING_CAPACITY
 
 class DistrCenter:
 
@@ -36,23 +32,50 @@ class DistrCenter:
             if ambulance.is_available:
                 return ambulance
         return None
+    
+    def drone_refresh(self, env):
+        for drone in self.drones:
+            if drone.current_battery < 0.25:
+                drone.is_available = False
+        
+        
+
+
 
     def process_blood_request(self, env, hospital, amount):
+        self.drone_refresh(env)
+
         with self.drones_resource.request() as req:
             t0 = env.now
+            
             yield req
+            
             drone = self.get_drone_highest_battery()
             drone.is_available = False
+            drones_needed = amount / DRONE_TOTAL_CARRYING_CAPACITY
+            print(drones_needed)
+            
+            print("initial battery: " + str(drone.charging_time()))
+            
             print("t={}\tDrone leaving base -- drone ID: {}, hospital ID: {}, amount: {}, time from request: {}".format(
                 str(env.now).zfill(3), drone.id, hospital.hospitalID, amount, env.now - t0))
+            
             yield env.timeout(round(drone.calculate_delivery_time(hospital, self)))
+            
             print("t={}\tBlood delivered -- drone ID: {}, hospital ID: {}, amount: {}, time from request: {}".format(
                 str(env.now).zfill(3), drone.id, hospital.hospitalID, amount, env.now-t0))
+            
             self.plot.add_deliver(env.now, amount, hospital.hospitalID, len(self.drones))
+            
             yield env.timeout(round(drone.calculate_delivery_time(hospital, self)))
+            
             print("t={}\tDrone back at base -- drone ID: {}, time from request: {}".format(
                 str(env.now).zfill(3), drone.id, env.now - t0))
+            
             drone.is_available = True
+            ## discharge drone based on the flight time / or kms
+            
+            print("final battery: " + str(drone.charging_time()))
 
         #with self.ambulances_resource.request() as req:
         #    t0 = env.now
--- vehicles/config.py
+++ vehicles/config.py
@@ -4,6 +4,7 @@ DRONE_CHARGING_SPEED = 0.5                  #h 0% -> 100%
 DRONE_CRUISING_SPEED = 101                  #km/h
 DRONE_CONSUMPTION_PER_KM = 0.00833          #kWh / km (1.25 kWh / 150 km)
 DRONE_EMISSIONS_PER_KM = 2.51               # grams of CO2 / km
+DRONE_SAFE_MIN_BATTERY = 0.25               # kWh
 
 AMBULANCE_TOTAL_CARRYING_CAPACITY = 1040    #kg
 AMBULANCE_EMISSIONS_PER_KM = 322            # grams of CO2 / km
