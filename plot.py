bee95e8738d6f13c2fc173494bfa541f51125c98
Francesco Romeo <francesco_romeo@me.com>
2021-11-22 23:58:28 +0100

    Edit to plot.py

--- plot.py
+++ plot.py
@@ -4,62 +4,42 @@ import pandas as pd
 class Plot:
     def __init__(self):
         self.requests = {}                  
-        self.delivers = {}                  # hospital: resource_amount: step: amount
-        self.partial_req = {}
-        self.partial_del = {}
-        self.emissions = []
-        self.costs = []
+        self.deliveries = {}
+        self.emissions = {}
+        self.costs = {}
 
     def add_request(self, step, amount, hospital, resource_amount):
-        if hospital not in self.requests.keys():
-            self.requests[hospital] = {}
-            self.partial_req[hospital] = {}
-        
-        if resource_amount not in self.requests[hospital].keys():
-            self.requests[hospital][resource_amount] = {}
-            self.partial_req[hospital][resource_amount] = {}
+        l = list(self.requests)
 
-        l = list(self.requests[hospital][resource_amount])
-
-        self.partial_req[hospital][resource_amount][step] = amount
-        if len(l) == 0:
-            self.requests[hospital][resource_amount][step] = amount
-        
+        if not self.requests:
+            self.requests[step] = amount
         else:
-            self.requests[hospital][resource_amount][step] = l[-1] + amount
+            self.requests[step] = l[-1] + amount
 
     def add_deliver(self, step, amount, hospital, resource_amount):
-        if hospital not in self.delivers.keys():
-            self.delivers[hospital] = {}
-            self.partial_del[hospital] = {}
-        
-        if resource_amount not in self.delivers[hospital].keys():
-            self.delivers[hospital][resource_amount] = {}
-            self.partial_del[hospital][resource_amount] = {}
 
-        l = list(self.delivers[hospital][resource_amount])
+        if resource_amount not in self.deliveries.keys():
+            self.deliveries[resource_amount] = {}
 
-        self.partial_del[hospital][resource_amount][step] = amount
+        l = list(self.deliveries[resource_amount])
+
         if len(l) == 0:
-            self.delivers[hospital][resource_amount][step] = amount
+            self.deliveries[resource_amount][step] = amount
         
         else:
-            self.delivers[hospital][resource_amount][step] = l[-1] + amount
+            self.deliveries[resource_amount][step] = l[-1] + amount
 
     def plot(self):
-        for hospital in self.requests.keys():
-            for r_amount in self.requests[hospital].keys():
-                df = pd.Series(self.requests[hospital][r_amount], name='Requested Blood Units - Hospital: ' + str(hospital))
-                df.index.name = 'Time'
-                df.reset_index()
-                df.plot(legend=True).set_ylabel("Blood Units")
+        df = pd.Series(self.requests, name='Requested Blood Units')
+        df.index.name = 'Time'
+        df.reset_index()
+        df.plot(legend=True).set_ylabel("Blood Units")
         
-        for hospital in self.delivers.keys():
-            for r_amount in self.delivers[hospital].keys():
-                df = pd.Series(self.delivers[hospital][r_amount], name='Delivered Blood Units - Hospital: ' + str(hospital) + "; Resources: " + str(r_amount))
-                df.index.name = 'Time'
-                df.reset_index()
-                df.plot(legend=True).set_ylabel("Blood Units")
+        for r_amount in self.deliveries.keys():
+            df = pd.Series(self.deliveries[r_amount], name='Delivered Blood Units; Resources: ' + str(r_amount))
+            df.index.name = 'Time'
+            df.reset_index()
+            df.plot(legend=True).set_ylabel("Blood Units")
 
         #for hospital in self.partial_req.keys():
         #    df = pd.Series(self.partial_req[hospital], name='Partials Blood Units Requests - Hospital: ' + str(hospital))
