0c4cb641aab7522611480767b101ae88fea53df6
kostas <kostaspalia95@hotmail.gr>
2021-11-24 15:22:30 +0100

    catastrophe generator

--- generators.py
+++ generators.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.stats as stats
+import random
 
 def generate_boold_demand_tseries_normal(amount_mean, amount_sigma, amount_min, amount_max,
                                          t_mean, t_sigma, t_min, t_max, length):
@@ -15,6 +16,8 @@ def generate_boold_demand_tseries_normal(amount_mean, amount_sigma, amount_min, 
     b = (t_max - t_mean) / t_sigma
     t_dist = stats.truncnorm(a, b, loc=t_mean, scale=t_sigma)
 
+
+
     # Generate time series
     t = 0
     while True:
@@ -23,4 +26,26 @@ def generate_boold_demand_tseries_normal(amount_mean, amount_sigma, amount_min, 
             ts[t] = amount_dist.rvs(1).astype(int)
         else:  # Finish if length of series is exceeded
             break
+    print(ts)
+    return ts
+
+def generate_boold_demand_tseries_catastrophe(amount_mean, amount_sigma, amount_min, amount_max, length):
+    ts = np.zeros(length)
+
+    # Create random distribution for blood amounts per surgery
+    a = (amount_min - amount_mean) / amount_sigma
+    b = (amount_max - amount_mean) / amount_sigma
+    amount_dist = stats.truncnorm(a, b, loc = amount_mean, scale = amount_sigma)
+
+    # For a pseudorandom time
+
+    a = round(0.25*length)
+    b = round(0.75*length)
+    t = random.randint(a, b)
+
+    #for a fixed time
+    #t = round(0.5*length)
+
+    #final time series
+    ts[t] = amount_dist.rvs(1).astype(int)
     return ts
--- main.py
+++ main.py
@@ -9,10 +9,16 @@ from hospital import Hospital
 from vehicles.drone import Drone
 from vehicles.ambulance import Ambulance
 from plot import Plot
-from generators import generate_boold_demand_tseries_normal
+from generators import generate_boold_demand_tseries_normal, generate_boold_demand_tseries_catastrophe
 from config import BLOOD_AMOUNT_MEAN_NORMAL, BLOOD_AMOUNT_SIGMA_NORMAL, BLOOD_AMOUNT_MIN_NORMAL, \
     BLOOD_AMOUNT_MAX_NORMAL, BLOOD_INTERVAL_MEAN_NORMAL, BLOOD_INTERVAL_SIGMA_NORMAL, BLOOD_INTERVAL_MIN_NORMAL, \
-    BLOOD_INTERVAL_MAX_NORMAL, DIST_CENTER_LONG, DIST_CENTER_LAT
+    BLOOD_INTERVAL_MAX_NORMAL, DIST_CENTER_LONG, DIST_CENTER_LAT, BLOOD_AMOUNT_MEAN_CATASTROPHE, \
+    BLOOD_AMOUNT_MIN_CATASTROPHE, BLOOD_AMOUNT_MAX_CATASTROPHE, BLOOD_AMOUNT_SIGMA_CATASTROPHE
+
+
+def BLOOD_AMOUNT_CATASTROPHE(args):
+    pass
+
 
 def run_testcase(hospitals_data_file, min_resources, max_resources, mode, sim_time):
 
@@ -22,9 +28,13 @@ def run_testcase(hospitals_data_file, min_resources, max_resources, mode, sim_ti
     # Generate time series for all hospitals in advance
     blood_req = []
     for i in range(hosp_data.shape[0]):
-        blood_req.append(generate_boold_demand_tseries_normal(
-            BLOOD_AMOUNT_MEAN_NORMAL, BLOOD_AMOUNT_SIGMA_NORMAL, BLOOD_AMOUNT_MIN_NORMAL, BLOOD_AMOUNT_MAX_NORMAL,
-            BLOOD_INTERVAL_MEAN_NORMAL, BLOOD_INTERVAL_SIGMA_NORMAL, BLOOD_INTERVAL_MIN_NORMAL, BLOOD_INTERVAL_MAX_NORMAL,
+        # blood_req.append(generate_boold_demand_tseries_catastrophe(
+        #     BLOOD_AMOUNT_MEAN_NORMAL, BLOOD_AMOUNT_SIGMA_NORMAL, BLOOD_AMOUNT_MIN_NORMAL, BLOOD_AMOUNT_MAX_NORMAL,
+        #     BLOOD_INTERVAL_MEAN_NORMAL, BLOOD_INTERVAL_SIGMA_NORMAL, BLOOD_INTERVAL_MIN_NORMAL, BLOOD_INTERVAL_MAX_NORMAL,
+        #     sim_time
+        # ))
+        blood_req.append(generate_boold_demand_tseries_catastrophe(
+            BLOOD_AMOUNT_MEAN_CATASTROPHE, BLOOD_AMOUNT_SIGMA_CATASTROPHE, BLOOD_AMOUNT_MIN_CATASTROPHE, BLOOD_AMOUNT_MAX_CATASTROPHE,
             sim_time
         ))
 
